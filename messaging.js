1fb077b54452fa0b4de5b8fca7d02de7f38ab5c5
Calvin Kinyanjui <158546364+CalKK@users.noreply.github.com>
2026-05-07 11:18:49 +0300

    Fix grammatical errors in email template

--- messaging.js
+++ messaging.js
@@ -6,7 +6,7 @@ const emailTemplate = `Dear Sir/Madam,
 
 I trust this message finds you well.
 
-I go by the name of Calvin Kinyanjui. I write this email with the view to lodge an application seeking a free Master of Ceremonies service at [Institution]'s corporate events.
+I go by the name of Calvin Kinyanjui. I write this email with the view to lodge an application seeking a free Master of Ceremony service at [Institution]'s corporate events.
 
 Kindly find attached below my contact details card.
 
@@ -14,7 +14,7 @@ In case of any queries, don't hesitate to reach out.
 
 Thank you & Kind Regards!`;
 
-const defaultSubject = 'Master of Ceremonies (MC) for your Corporate Events for free - Calvin Kinyanjui'
+const defaultSubject = 'Master of Ceremony (MC) for your Corporate Events for free - Calvin Kinyanjui'
 
 /**
  * Create a Nodemailer transporter using Gmail SMTP
